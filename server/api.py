5d51af037b7a58657bf9a8b98459460f66f527c9
conifer0us <cnferg04@gmail.com>
2022-05-23 01:27:36 -0500

    added waitress wsgi server library for production use

--- server/api.py
+++ server/api.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template, request, send_file
 import hashlib
 from os.path import exists
 
-# Change to Alter Password Salt
+# Change to Alter Password Salt. Admin accounts will lose access if this is done with passwords already set.
 salt = "naohoinvoaisouaoiweniojfoihas88920903"
+
+# Port that Flask Application will listen on.
+app_port = 443
+
+# Basic Flask Application Creation
 app = Flask(__name__)
 
 # Returns a Basic skPOS Welcome Page
@@ -24,7 +29,7 @@ def developerSignIn():
 # Returns a png representation of the SKPOS Logo
 @app.route("/logoDark.png", methods=['GET'])
 def returnDarkLogoSvg():
-	return send_file("./templates/logoDark.png", mimetype='image/png')
+	return send_file("./images/logoDark.png", mimetype='image/png')
 
 # Registers an Ordering Device (Possible from the Developer Dashboard)
 @app.route("/registerOrderDevice", methods=['GET', 'POST'])
@@ -37,11 +42,21 @@ def hash(information : str, salt : str) -> str:
 	return hashlib.md5(str2hash.encode()).hexdigest()
 
 # Creates the "admin_pass.db" file to store an admin username and password if not already created
-if not exists("admin_pass.db"):
+def addAdminLogin():
 	admin_user = input("Enter Admin Username: ")
 	admin_password = input("Enter Admin Password: ")
 	with open("admin_pass.db", "a") as adminFile:
 		adminFile.write(admin_user + ":" + hash(admin_password, salt=salt)+"\n")
 
 # Runs the Flask Application on Port 443 
-app.run(port=443)
+if __name__ == "__main__":
+	import sys
+	if not exists("admin_pass.db"):
+		addAdminLogin()
+	if len(sys.argv) < 2:
+		from waitress import serve
+		serve(app, host="0.0.0.0", port=app_port)
+	elif sys.argv[1] == "debug":
+		app.run(debug=True, port=app_port)
+	else:
+		print("Unrecognized Arguments.\nSupply no arguments for a production server.\nSupply 'debug' as the first argument for a flask development server.")
