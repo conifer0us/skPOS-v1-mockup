1697265b82a5e8a182c3bfb30e896df9da29e99a
conifer0us <cnferg04@gmail.com>
2022-06-05 16:08:13 -0500

    Removed Test Menus and Began Creating SKPOS Menus

--- SKPOSandroid/app/src/main/java/com/subkingofmobile/skpos_android/StartMenu.kt
+++ SKPOSandroid/app/src/main/java/com/subkingofmobile/skpos_android/StartMenu.kt
@@ -5,15 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-import com.subkingofmobile.skpos_android.databinding.FragmentSecondBinding
+import com.subkingofmobile.skpos_android.databinding.StartMenuBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class StartMenu : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: StartMenuBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,21 +27,21 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = StartMenuBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun changeConfigurationText(newText : String) {
+        binding.
     }
 }
