ff355b1545d1f800fe4b2da80174116336a918b6
conifer0us <cnferg04@gmail.com>
2022-06-12 10:07:23 -0500

    Updated Settings Fragment

--- SKPOSandroid/app/src/main/java/com/subkingofmobile/skpos_android/SettingsFragment.kt
+++ SKPOSandroid/app/src/main/java/com/subkingofmobile/skpos_android/SettingsFragment.kt
@@ -1,9 +1,15 @@
 package com.subkingofmobile.skpos_android
 
+import android.content.Context
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment(appContext : Context) : PreferenceFragmentCompat() {
+    private val settingsManager : SettingsManager
+
+    init {
+        settingsManager = SettingsManager(appContext)
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
