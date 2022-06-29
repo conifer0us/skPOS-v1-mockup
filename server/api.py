9d6e4652f63f9ed625518caed6821a50eec96cb5
conifer0us <cnferg04@gmail.com>
2022-06-29 02:31:52 -0500

    Add Support for Device Registration Using QR Codes from the Admin Panel

--- server/api.py
+++ server/api.py
@@ -1,10 +1,11 @@
 import json
+from xmlrpc.client import Boolean
 from flask import Flask, jsonify, make_response, redirect, render_template, request, send_file
 import hashlib
 from os.path import exists
 from random import getrandbits
 
-# Change to Alter Password Salt. Admin accounts will lose access if this is done with passwords already set.
+# Change to Alter Password Salt. Admin accounts will lose access if this is done with passwords already set. Registered Ordering Devices will also become unregistered
 salt = "naohoinvoaisouaoiweniojfoihas88920903"
 
 # Port that Flask Application will listen on.
@@ -15,6 +16,12 @@ app = Flask(__name__)
 
 # Creates Non-Persistent Admin Cookie Storage (hashed with salt)
 adminCookies = []
+
+# Defines the Name of the file to store Device IDs in
+deviceRegistrationFile = "devices.db"
+
+# Defines the Name of the file to store Admin Login information in
+adminLoginFile = "admin_login.db"
 
 # Returns a Basic skPOS Welcome Page
 @app.route("/servertest", methods=['GET'])
@@ -29,7 +36,7 @@ def developerSignIn():
 	elif request.method == 'GET':
 		return render_template("developerlogin.html")
 	elif request.method == 'POST':
-		if request.form["uname"] + ":" + hash(request.form["pwd"], salt=salt) + "\n" in open("admin_login.db", "r").readlines():
+		if request.form["uname"] + ":" + hash(request.form["pwd"], salt=salt) + "\n" in open(adminLoginFile, "r").readlines():
 			newCookieVal = getrandbits(64)
 			createCookie(newCookieVal)
 			resp = make_response("Accepted", 200)
@@ -56,7 +63,16 @@ def returnFavicon():
 # Registers an Ordering Device (Possible from the Developer Dashboard)
 @app.route("/registerOrderDevice", methods=['POST'])
 def registerOrderDevice(): 
-	return ""
+	if not isAdmin(request):
+		return jsonify({"err", "Access Denied"}), 401
+	submittedDeviceID = request.json["deviceID"]
+	if isRegisteredDevice(request):
+		return jsonify({"err", "Device Already Registered"}), 409
+	if len(submittedDeviceID) != 256:
+		return jsonify({"err", "Server Could Not Process Device ID"}), 500
+	else: 
+		addOrderingDeviceToDB(submittedDeviceID)
+		return "", 200
 
 # Removes all valid login cookies and forces admins to log back in
 @app.route("/adminlogout", methods=["POST"])
@@ -74,12 +90,17 @@ def hash(information : str, salt : str) -> str:
 	str2hash = information + salt
 	return hashlib.md5(str2hash.encode()).hexdigest()
 
-# Creates the "admin_login.db" file to store an admin username and password if not already created
+# Creates the file specified by adminLoginFile variable to store an admin username and password if not already created
 def addAdminLogin():
 	admin_user = input("Enter Admin Username: ")
 	admin_password = input("Enter Admin Password: ")
-	with open("admin_login.db", "a") as adminFile:
+	with open(adminLoginFile, "a") as adminFile:
 		adminFile.write(admin_user + ":" + hash(admin_password, salt=salt)+"\n")
+
+# Adds hashed device IDs to the file specified by the deviceRegistrationFile variable (creates file if it does not exist)
+def addOrderingDeviceToDB(deviceID : str):
+	with open(deviceRegistrationFile, "a") as deviceFile:
+		deviceFile.write(hash(deviceID, salt=salt))
 
 # Checks if a specific request was sent by an already logged in admin user
 def isAdmin(req : request):
@@ -90,6 +111,15 @@ def isAdmin(req : request):
 		return True
 	return False
 
+def isRegisteredDevice(req : request) -> Boolean:
+	deviceID = req.json["deviceID"]
+	if deviceID is None:
+		return False
+	elif hash(deviceID, salt=salt)+"\n" in open(deviceRegistrationFile, "r").readlines():
+		return True
+	else: 
+		return False
+
 # Adds a value as a valid cookie
 def createCookie(val):
 	adminCookies.append(hash(str(val), salt=salt))
@@ -97,7 +127,7 @@ def createCookie(val):
 # Runs the Flask Application on Port 443 
 if __name__ == "__main__":
 	import sys
-	if not exists("admin_login.db"):
+	if not exists(adminLoginFile):
 		addAdminLogin()
 	if len(sys.argv) < 2:
 		from waitress import serve
